a9183861294e153eb583ad140016d284f21ccd0c
Dexter <dexter@dxlb.nl>
2022-05-09 13:21:45 +0200

    Fix bug in wipe

--- src/lib/Stage.svelte
+++ src/lib/Stage.svelte
@@ -15,17 +15,18 @@
     rgbSpots.update(spots => spots.map(() => color))
     console.log('filled', color)
   }
+</script>
+
+<script lang='ts'>
+  let count = 0
 
   const sendSacn = async (data) => {
     console.log("Sending", data)
 		const response = await invoke('send_sacn', {
-			data
+			data: data.map(v => v|0)
 		})
+    count ++
   }
-
-</script>
-
-<script lang='ts'>
   $: {
     // Should be calling send on max hz
     console.log("Update", $rgbSpots, $spots)
@@ -33,18 +34,22 @@
       ...$rgbSpots.flatMap(color => [0, 0, ...color.rgb().array()]),
       ...$spots
     ])
-      .then(() => console.log('sent data'))
+      .then(() => {})
       .catch(e => console.error('error', e))
   }
 </script>
 
 <svg viewBox="0 0 8 12">
+  <text y=1 fill=white>{count%1000}</text>
   {#each $rgbSpots as color, i}
     <circle r=1 cx={i%4 * 2 + 1} cy={12 - ((i/4|0) * 2 + 1)} fill="{color}" />
   {/each}
 </svg>
 
 <style>
+  text {
+    font-size: 1px;
+  }
   svg {
     background: gray;
     width: 8rem;
